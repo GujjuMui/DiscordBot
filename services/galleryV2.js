06459ca4288b77bfb7d316376d56e544ec715484
GujjuMui <vaibhavchoudhary1978@gmail.com>
2026-09-19 21:48:40 +0530

    refactor: harden gallery state service

--- services/galleryV2.js
+++ services/galleryV2.js
@@ -1,71 +1,50 @@
 const galleries = new Map();
 
 function open(messageId, data) {
-
     galleries.set(messageId, {
         ...data,
-        index: 0
+        index: 0,
     });
-
 }
 
 function get(messageId) {
-
     return galleries.get(messageId);
-
 }
 
 function setIndex(messageId, index) {
-
     const gallery = galleries.get(messageId);
-
     if (!gallery) return;
 
     gallery.index = index;
-
 }
 
 function next(messageId) {
-
     const gallery = galleries.get(messageId);
+    if (!gallery || !gallery.items?.length) return;
 
-    if (!gallery) return;
-
-    gallery.index =
-        (gallery.index + 1) % gallery.items.length;
-
+    gallery.index = (gallery.index + 1) % gallery.items.length;
     return gallery;
-
 }
 
 function previous(messageId) {
-
     const gallery = galleries.get(messageId);
+    if (!gallery || !gallery.items?.length) return;
 
-    if (!gallery) return;
-
-    gallery.index--;
-
-    if (gallery.index < 0)
-        gallery.index = gallery.items.length - 1;
+    gallery.index =
+        (gallery.index - 1 + gallery.items.length) % gallery.items.length;
 
     return gallery;
-
 }
 
 function close(messageId) {
-
     galleries.delete(messageId);
-
 }
 
 module.exports = {
-
     open,
     get,
     setIndex,
     next,
     previous,
-    close
-
+    close,
 };
